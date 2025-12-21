8ee61a1e9820bb1459280b98d03ca3a0b1a3247d
Ircham Aji Nugroho <irchamajin@gmail.com>
2025-12-21 21:29:30 +0700

    adjust meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,7 +18,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Finance Calculator - Income Allocation',
+        title: 'Income Allocation',
       },
       {
         name: 'description',
